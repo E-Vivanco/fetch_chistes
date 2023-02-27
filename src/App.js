d3320982a9f469640a6fd65d862777e1e28c2175
E-Vivanco <vivanco.elsa@gmail.com>
2023-02-27 05:48:17 +0000

    ajustes 2°

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import {useState,useEffect} from 'react';
 import './App.css';
 
 function App() {
+const [chistes,setChistes]=useState([])
+const myStyle={
+  border: "0 px solid",
+  borderRadius: "15px"
+}
+useEffect(() => {
+  const url='https://api.chucknorris.io/jokes/random';
+  const peticion1=fetch(url);
+  peticion1
+  .then(chiste => chiste.json())
+  .then(lectura =>{
+    setChistes(lectura)
+  })
+  
+  .catch((e)=>console.error(e))
+}, []);
+  
+console.log("soy chistes",chistes)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container my-5 mb-3 py-5">
+      <h2>Chistes</h2>
+      <div className='w-100 bg-warning  mx-0 px-2 ' style={myStyle}>{chistes.value}</div>
+      
     </div>
   );
 }
